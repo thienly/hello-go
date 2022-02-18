5514c97a7157eef510d8983d8e9477467e25e76b
thienly <phuthien0308@gmail.com>
2022-02-18 16:54:49 +0700

    service

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	for {
+		name := flag.String("name", "hello world", "name description")
+		flag.Parse()
 		time.Sleep(1 * time.Second)
 		log.Println("Hello world!!!")
+		sigs := make(chan os.Signal)
+		signal.Notify(sigs, syscall.SIGQUIT)
+		go func() {
+			s := <-sigs
+			log.Printf("RECEIVED SIGNAL: %s", s)
+			os.Exit(1)
+		}()
+		for {
+			log.Printf("hello %s", *name)
+			time.Sleep(1 * time.Second)
+		}
 	}
 }
